0c4690376fc34fd857b7a88195c819831e2ec40b
Ken Williams <Ken.Williams@WindLogics.com>
2018-12-17 15:13:25 -0600

    Simplify RPairlist by using LinkedHashMap

--- src/main/java/com/windlogics/r/serialize/RPairlist.java
+++ src/main/java/com/windlogics/r/serialize/RPairlist.java
@@ -3,49 +3,45 @@ package com.windlogics.r.serialize;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RPairlist extends RThing<Object> {
     // This class is used for attributes too
     public RPairlist() { this(new ArrayList<>()); }
     public RPairlist(List<Object> d) { this(d, null); }
-    public RPairlist(List<Object> d, RPairlist attrs) { super(d, attrs); }
+    public RPairlist(List<Object> d, RPairlist attrs) {
+        super(d, attrs);
+        data = null;  // Shouldn't be used
+        content = new LinkedHashMap<>();
+    }
+
+    protected Map<String, RThing> content;
 
     @Override public void putData(DataOutputStream os) throws IOException {
-        for (int i=0; i<data.size(); i+=2) {
+        for (Map.Entry<String, RThing> e : content.entrySet()) {
             os.writeInt(0x402);
             os.writeInt(1);
             os.writeInt(RString.getStringStart());
-            String key = (String) data.get(i);  // Name
+            String key = e.getKey();  // Name
             os.writeInt(key.length());
             os.write(key.getBytes());
-            ((RThing) data.get(i+1)).serialize(os);        // Value
+            e.getValue().serialize(os);        // Value
         }
         new RNull().serialize(os);
     }
 
     public int length() {
-        return data.size() / 2;
+        return content.size();
     }
 
     public boolean hasKey(String key) {
-
-        for (int i = 0; i < data.size(); i += 2) {
-            if (data.get(i).equals(key))
-                return true;
-        }
-        return false;
+        return content.containsKey(key);
     }
 
     @Override public RPairlist setAttr(String key, RThing value) {
-        for (int i = 0; i < data.size(); i += 2) {
-            if (data.get(i).equals(key)) {
-                data.set(i+1, value);
-                return this;
-            }
-        }
-        data.add(key);
-        data.add(value);
+        content.put(key, value);
         return this;
     }
 }
